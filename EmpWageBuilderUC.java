4af33f6c5d87643cf2b049d647531d671f2e53fe
softtech <amols424@gmail.com>
2021-02-09 08:57:24 +0530

    Added :Switch case stmt

--- EmpWageBuilderUC.java
+++ EmpWageBuilderUC.java
@@ -5,18 +5,17 @@ int IS_PART_TIME=2;
 int WAGE_PER_HR=20;
 int DAY_HRS=16;
 int PART_TIME_HRS=8;
-double empCheck=Math.floor(Math.random()*10)%3;
-if (empCheck==IS_FULL_TIME)
-{
-System.out.println("Employee is present");
-System.out.println("Employee Daily wage is" + WAGE_PER_HR*DAY_HRS );
-}
-else if(empCheck==IS_PART_TIME)
-{
-System.out.println("Employee is part time"+WAGE_PER_HR*PART_TIME_HRS);
-}
-else {
-System.out.println("Employee is Absent");
+double empCheck=Math.floor((Math.random()*10)%3);
+int ch=(int)empCheck;
+switch (ch){
+ 			case 1:
+   	System.out.println("Employee is full time wage is"+DAY_HRS*WAGE_PER_HR);
+					break;
+			case 2:
+ System.out.println("Employee is part time and wage is "+PART_TIME_HRS*WAGE_PER_HR);
+						break;
+default:
+System.out.println("Employee is absent");
 }
 }
 }
